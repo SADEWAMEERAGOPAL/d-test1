44d5af4a4fdaf6bb3c936f57c62f8f4fc0db70be
SADEWAMEERAGOPAL <SADEWADMEERA76@GMAIL.COM>
2025-09-10 15:04:30 +0530

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,40 @@
+let cl=console.log;
+ const setZero=(num)=>{
+	if(num <10){
+		return "0" + num
+	} 
+	else{
+		return num
+	}
+ }
+ 
+ const digitalClock=()=>{
+	 let digital=document.getElementById("clock");
+	 let d=new Date();
+	 cl(d)
+	 let hr=d.getHours();
+	 let min=d.getMinutes();
+	 let sec=d.getSeconds();
+	 
+	 let session ="AM";
+	 
+	 if(hr>12){
+		 hr=hr-12;
+		 session="PM";
+	 }
+	 
+	 let hrWithZero=setZero(hr)
+	 let minWithZero=setZero(min)
+	 let secWithZero=setZero(sec)
+	 
+	let result=`${hrWithZero} : ${minWithZero} : ${secWithZero}: ${session}` 
+	setTimeout(digitalClock, 1000)
+	digital.innerHTML=result;
+ 
+ }
+ digitalClock();
+ 
+ 
+ 
+ 
+ 
